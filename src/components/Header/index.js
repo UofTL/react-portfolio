eef92b766b23034318d9588a534e036e9365dbe6
Eleonore <eleonore.caclard@gmail.com>
2021-11-09 08:46:52 -0500

    in the Header Redirect replace by Link

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {Route, Redirect, HashRouter} from "react-router-dom";
+import {Route, Link, HashRouter} from "react-router-dom";
 import Navigation from "../../components/Navigation";
 import About from "../../components/About";
 import Contact from "../../components/Contact";
@@ -17,7 +17,7 @@ return (
 
 
 <div className = "content">
-<Route exact path = "/" render = {() => ( < Redirect to = "/portfolio"/> )}/> 
+<Route exact path = "/" render = {() => ( < Link to = "/portfolio"/> )}/> 
 <Route path = "/portfolio"component = {Portfolio}/> 
 <Route path = "/about" component = {About}/> 
 <Route path = "/contact" component = {Contact}/> 
