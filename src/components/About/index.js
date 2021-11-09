4fc7e732035252126a3e9fe5089c139ef42e83a9
Eleonore <eleonore.caclard@gmail.com>
2021-11-08 22:23:55 -0500

    index.js issues from components folder resolved

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,39 +1,35 @@
-import React from 'react';
+import React from "react";
 
 function About() {
-    return ( <
-        section className = "title" >
-        <
-        h1 class = "name" > about: Eleonore - Gabrielle Caclard < /h1> <
-        hr > < /hr>
-
-        <
-        div className = "row justify-content-center" >
-        <
-        div className = "col-10"
-        id = "about-section" >
-        <
-        img class = "mb-5"
-        src = "https://github.com/UofTL/react-portfolio/raw/master/src/assets/images/Eleonore-Gabrielle-Caclard.jpg"
-        alt = "Eleonore-Gabrielle-Caclard" / >
-        <
-        p >
-        Hi, I am Eleonore - Gabrielle Caclard.You can also call me 爱丽 <
-        /p> <
-        p >
-        I am 50 % Rwandan and 50 % European, the mix made me 100 % awesome <
-        /p> <
-        p >
-        I have lived in many countries on 4 different continents speak(FR / EN) and understand few others languages <
-        /p> <
-        p >
-        View full < a href = "#/resume"
-        class = "link" > resume < /a> < /
-        p > <
-        /div>   < /
-        div > <
-        /section> 
-    )
+  return (
+    <section className="title">
+      <h1 class="name"> about: Eleonore-Gabrielle Caclard</h1>
+      <hr></hr>
+      <div className="row justify-content-center">
+        <div className="col-10" id="about-section">
+          <img
+            class="mb-5"
+            src="https://github.com/UofTL/react-portfolio/raw/master/src/assets/images/Eleonore-Gabrielle-Caclard.jpg"
+            alt="Eleonore-Gabrielle Caclard"
+          />
+          <p>Hi, I am Eleonore-Gabrielle Caclard.You can also call me 爱丽</p>
+          <p>
+            I am 50 % Rwandan and 50 % European, the mix made me 100 % awesome
+          </p>
+          <p>
+            I have lived in many countries on 4 different continents speak
+            English and French and understand few others languages
+          </p>
+          <p>
+            View full
+            <a href="#/resume" class="link">
+              resume
+            </a>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default About;
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -36,64 +36,37 @@ function ContactForm() {
         e.preventDefault();
     }
 
-    return ( <
-        section class = "justify-content-center"
-        id = "contact-section" >
-        <
-        h1 data - testid = 'h1tag'
-        className = "contact" > contact: Eleonore - Gabrielle Caclard < /h1> <
-        hr > < /hr> <
-        form class = "justify-content-center"
-        id = "contact-form" >
-        <
-        div >
-        <
-        label htmlFor = "name" > name: < /label> <
-        input class = "form-control"
-        type = "text"
-        name = "name"
-        defaultValue = { name }
-        onBlur = { handleChange }
-        /> <
-        /div> <
-        div >
-        <
-        label htmlFor = "email" > email: < /label> <
-        input class = "form-control"
-        type = "email"
-        name = "email"
-        defaultValue = { email }
-        onBlur = { handleChange }
-        /> <
-        /div> <
-        div >
-        <
-        label htmlFor = "message" > message: < /label> <
-        textarea class = "form-control"
-        name = "message"
-        defaultValue = { message }
-        onBlur = { handleChange }
-        rows = "7" / >
-        <
-        /div>  {
-            errorMessage && ( <
-                div >
-                <
-                p className = "error-text" > { errorMessage } < /p> <
-                /div>
+    return ( 
+        <section class = "justify-content-center"id = "contact-section">
+        <h1 data-testid = 'h1tag' className = "contact"> contact: Eleonore-Gabrielle Caclard</h1>
+         <hr></hr> 
+        <form class = "justify-content-center"id = "contact-form">
+        <div>
+        <label htmlFor = "name">name:</label> 
+        <input class = "form-control"type = "text"name = "name" defaultValue={name} onBlur={handleChange}/> 
+        </div> 
+        <div>
+        <label htmlFor = "email" > email: </label> 
+        <input class = "form-control" type = "email" name = "email" defaultValue={email} onBlur={handleChange}/> 
+        </div> 
+        <div>
+        <label htmlFor = "message">message: </label> 
+        <textarea class = "form-control" name = "message" defaultValue = {message} onBlur = {handleChange} rows = "7"/>
+        </div>  
+        {errorMessage && ( 
+            <div>
+                <p className = "error-text"> {errorMessage} 
+                </p> 
+                </div>
             )
         }
 
-        <
-        div >
-        <
-        button data - testid = 'button'
-        class = "btn btn-outline-dark mt-4"
-        type = "submit"
-        onSubmit = { handleSubmit } > Submit < /button> <
-        /div> <
-        /form> <
-        /section>
+        <div>
+        <button data-testid = 'button'class = "btn btn-outline-dark mt-4"
+        type = "submit" onSubmit = { handleSubmit } > Submit </button> 
+        </div> 
+        </form> 
+        </section>
     );
 }
 
